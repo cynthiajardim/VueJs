c8fa03dd263b8b876bd3a01897daf23c01fd2974
Cynthia <cynthiaareasjardim@gmail.com>
2024-01-07 17:40:50 -0300

    Último desafio do  módulo de DOM

--- dom/exercicios4/app.js
+++ dom/exercicios4/app.js
@@ -1,14 +1,28 @@
 new Vue({
 	el: '#desafio',
 	data: {
+		classe1: 'destaque',
+		corBackground: 'c1',
+		largura: 'tamanhoM',
+		classeUsuario: '',
+		corQuadrado: 'c1',
+		aplicarClasse: false,
+		width: 0
 
 	},
 	methods: {
 		iniciarEfeito() {
-
+			setInterval(() => {
+				this.classe1 = this.classe1 == 'destaque' ? 'encolher' : 'destaque'
+			}, 1000)
 		},
 		iniciarProgresso() {
-
+			let valor = 0;
+			const temporizador = setInterval(() => {
+				valor  += 5
+				this.width = `${valor}%`
+				if(valor == 100) clearInterval(temporizador)
+			}, 500)
 		}
 	}
 })
